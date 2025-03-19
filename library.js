c514aaf261452e5a4647169d97b4d50b73232912
Matthew Brash <matthew.brash@ocsb.ca>
2025-03-19 19:03:52 -0400

    Quick update to the header in library.js

--- library.js
+++ library.js
@@ -1,3 +1,16 @@
+/**
+ *  Author: Mr. Squirrel 🐿️
+ * 
+ *  A useful library of various functions.
+ *  Feel free to copy them into your code file or 
+ *  import this library using:
+ *      const lib = require("./library.js");
+ *  and then:
+ *      lib.randInt(0, 10);     // for example
+ *   or lib.round(Math.PI, 2);
+ */
+
+
 'use strict';
 
 /**
